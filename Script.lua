702e4d3862be6304687a4ca31d10ddce4c265cd1
o5u3 <69703353+o5u3@users.noreply.github.com>
2022-07-04 03:25:12 -0700

    Old Script

---
+++ Script.lua
@@ -0,0 +1,6 @@
+setclipboard([[-- Options --
+_G.apDefault = false -- Setting this to true will enable the auto-player by default on execute
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/o5u3/Friday-Night-Bloxxin-Autoplayer/main/c4855267.lua"))()]])
+
+game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "New Script has been copied.", Duration = 4,})
