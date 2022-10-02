231191dac3162adabf2b1a10c3dc8a8b86eaa462
o5u3 <69703353+o5u3@users.noreply.github.com>
2022-10-02 12:21:25 -0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,8 +1,9 @@
 setclipboard([[-- Options --
+_G.LegacyUI = false -- Enable if you're having execution issues
 _G.apDefault = false -- Setting this to true will enable the auto-player by default on execute
 _G.joinDiscord = false -- Setting this to true will make you join the discord server on execute
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/o5u3/Friday-Night-Bloxxin-Autoplayer/main/B96625273F7301ABBF0325835CD829222CAE6F1D9BCD1889862E728B7521859CE2C50C78842D718D85A2F4F56F6D08CFB2F6CFA5B7587F76F0E9078148E05062.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/o5u3/Friday-Night-Bloxxin-Autoplayer/main/C26201D86CAF3855DD117C3B47AA903CBB84AE1FA9E212BD04FC604B5089302D240CE9BDF0761C9A39A678D655DCC45E53055190FF0DA149EFA52D406AF5749E.lua"))()
 ]])
 
 game.StarterGui:SetCore("SendNotification", {Title = "Notification", Text = "New script copied.", Duration = 4,})
